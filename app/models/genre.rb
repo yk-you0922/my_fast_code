40a10eeac8f96462ddeef2b4e5e4adf9def044a4
荒見友稀 <aramiyuki@YK-MacBook-Air.local>
2020-08-10 12:42:18 +0900

    [fix association]

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,6 +1,6 @@
 class Genre < ApplicationRecord
   # カテゴリーを通してポストを取得、上下させない
-  has_many :categories
+  has_many :categories, dependent: :destroy
   has_many :posts, through: :categories, dependent: :destroy
 
   # バリデーション
--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,7 @@ class Post < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :comments, dependent: :destroy
   # カテゴリーを通してジャンルを取得するので、上下させない
-  has_many :categories
+  has_many :categories, dependent: :destroy
   has_many :genres, through: :categories
 
   attachment :image
