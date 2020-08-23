acfa05e79e2e6d76a445620a8186fa1e869cfce0
vagrant <vagrant@localhost.localdomain>
2020-08-23 07:20:31 +0000

    [devise.rb fix]

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -282,7 +282,8 @@ Devise.setup do |config|
   config.omniauth :twitter,
                   ENV['TWITTER_API_KEY'],
                   ENV['TWITTER_API_SECRET'],
-                  scope: 'email'
+                  scope: 'email',
+                  callback_url: "https://my-fast-code.work/users/auth/twitter/callback"
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
