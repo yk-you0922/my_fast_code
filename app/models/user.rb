ad423e3e8a9896a314fa214144bf1c2620b07bbd
vagrant <vagrant@localhost.localdomain>
2020-08-23 05:26:46 +0000

    [fix dotenv]

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:google, :twitter]
-          # omniauth_providers: %i(google)
 
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
