2623ea981500acf3246648ec86d977a8d0469a36
荒見友稀 <aramiyuki@YK-MacBook-Air.local>
2020-08-27 13:40:25 +0900

    [fix user.rb_contacts_dependent: :destroy]

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
 
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
-  has_many :contacts
+  has_many :contacts, dependent: :destroy
   has_many :favorites, dependent: :destroy
   has_many :favorites_posts, through: :favorites, source: :post
   has_many :comment_favorites, dependent: :destroy
